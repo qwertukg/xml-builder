fcaa91b9278505f4b9f6353e6dfb358ec17c8bf8
44000567 <daniil.rakhmatulin@gmail.com>
2018-09-25 10:13:12 +0600

    init

---
+++ src/main/java/kz/qwertukg/xmlBuilder/xmlBulider.kt
@@ -0,0 +1,48 @@
+package kz.qwertukg.xmlBuilder
+
+import java.lang.StringBuilder
+
+@DslMarker
+annotation class TagMarker
+
+const val TAB = "    "
+
+data class Attribute(val name: String, val value: String)
+
+@TagMarker
+interface Tag {
+    var name: String
+    val attributes: MutableList<Attribute>
+
+    fun attr(name: String, value: String) = attributes.add(Attribute(name, value))
+
+    fun render(margin: Int = 0): String
+
+    fun renderOpeningTag() = StringBuilder().apply {
+        append("<$name")
+        attributes.forEach { append(" ${it.name}=\"${it.value}\"") }
+        append(">")
+    }
+}
+
+data class TagFather(override var name: String, override val attributes: MutableList<Attribute> = mutableListOf(), val tags: MutableList<Tag> = mutableListOf()) : Tag {
+    fun tag(name: String, block: TagFather.() -> Unit) = tags.add(TagFather(name).apply(block))
+
+    fun tag(name: String, value: String, block: TagValue.() -> Unit = {}) = tags.add(TagValue(name, value).apply(block))
+
+    override fun render(margin: Int): String = StringBuilder().apply {
+        appendln(TAB.repeat(margin) + renderOpeningTag())
+        tags.forEach { appendln(it.render(margin + 1)) }
+        append(TAB.repeat(margin) + "<$name/>")
+    }.toString()
+}
+
+data class TagValue(override var name: String, val value: String, override val attributes: MutableList<Attribute> = mutableListOf()) : Tag {
+    override fun render(margin: Int) = StringBuilder().apply {
+        append(TAB.repeat(margin) + renderOpeningTag())
+        append(value)
+        append("<$name/>")
+    }.toString()
+}
+
+fun tag(name: String, block: TagFather.() -> Unit) = TagFather(name).apply(block)
---
+++ src/test/java/kz/qwertukg/xmlBuilder/BasicTest.kt
@@ -0,0 +1,51 @@
+package kz.qwertukg.xmlBuilder
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class BasicTest {
+
+    @Test
+    fun `Check is XML`() {
+        val actualXml = tag("html") {
+            tag("head") {
+                tag("title", "Is XML?")
+            }
+            tag("body") {
+                tag("h1", "Hello world!")
+                tag("p") {
+                    tag("a", "Kotlin") {
+                        attr("href", "https://kotlinlang.org")
+                        attr("target", "_blank")
+                    }
+                }
+                tag("p") {
+                    tag("img") {
+                        attr("src", "kotlin.png")
+                    }
+                }
+            }
+        }.render()
+
+        val expectedXml = """
+            <html>
+                <head>
+                    <title>Is XML?<title/>
+                <head/>
+                <body>
+                    <h1>Hello world!<h1/>
+                    <p>
+                        <a href="https://kotlinlang.org" target="_blank">Kotlin<a/>
+                    <p/>
+                    <p>
+                        <img src="kotlin.png">
+                        <img/>
+                    <p/>
+                <body/>
+            <html/>
+        """.trimIndent().replace("\n", System.lineSeparator())
+
+        assertEquals(expectedXml, actualXml)
+    }
+
+}
